03d9fa0162457139772e5cd2760cfc8dc23f3630
Mei Gao <xuga@microsoft.com>
2020-10-12 10:21:31 -0700

    Add mlflow to log metrics

--- test_itp.py
+++ test_itp.py
@@ -1,8 +1,13 @@
 import logging
 import os
+import mlflow
+
 
 
 # if __name__ == "__main__":
 logging.info("This is from logging info %d", __file__)
 print("print results here")
-print("print dir", os.listdir("/data/"))
+dataset = "/DATASET/luna16"
+mlflow.log_metric("precision", 0.5)
+
+print("print dir", os.listdir(dataset))
