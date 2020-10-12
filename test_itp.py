70d12c51509a50947f48656f5971b469d02571be
Mei Gao <xuga@microsoft.com>
2020-10-12 09:30:46 -0700

    fix unasigned variable

--- test_itp.py
+++ test_itp.py
@@ -3,6 +3,6 @@ import os
 
 
 # if __name__ == "__main__":
-logging.info("This is from logging info %d", a)
+logging.info("This is from logging info %d", __file__)
 print("print results here")
 print("print dir", os.listdir("/data/"))
