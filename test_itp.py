a6cb9d4e78fa987aacec8684fd8348158eefd593
Mei Gao <xuga@microsoft.com>
2020-10-12 11:31:18 -0700

    Add log metrics

--- test_itp.py
+++ test_itp.py
@@ -1,13 +1,14 @@
 import logging
 import os
-import mlflow
+from azureml.core.run import Run
 
+run = Run.get_context()
 
 
 # if __name__ == "__main__":
 logging.info("This is from logging info %d", __file__)
 print("print results here")
 dataset = "/DATASET/luna16"
-mlflow.log_metric("precision", 0.5)
+run.log("precision", 0.5)
 
-print("print dir", os.listdir(dataset))
+# print("print dir", os.listdir(dataset))
