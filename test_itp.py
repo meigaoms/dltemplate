44017fb0e3f03255413b3494d7f823588d8cbe7c
Mei Gao <xuga@microsoft.com>
2020-10-12 14:07:06 -0700

    fix logging

--- test_itp.py
+++ test_itp.py
@@ -6,7 +6,7 @@ run = Run.get_context()
 logging.basicConfig(level=logging.DEBUG)
 
 # if __name__ == "__main__":
-logging.info("This is from logging info %d", __file__)
+logging.info("This is from logging info %s", __file__)
 print("print results here")
 dataset = "/DATASET/luna16"
 run.log("precision", 0.5)
